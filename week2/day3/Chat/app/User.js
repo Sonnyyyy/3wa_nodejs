a9b65192b9db22e082bfcf2aef5d0226778f303e
Sonny <sonnypollet@hotmail.fr>
2023-04-26 16:32:19 +0200

    liste user

---
+++ week2/day3/Chat/app/User.js
@@ -0,0 +1,17 @@
+const ent = require('ent')
+
+class User {
+    constructor(socket, nickname) {
+        this.id        = socket.id
+        this.nickname  = ent.encode(nickname)
+        this.socket    = socket
+    }
+
+    destroy() {
+        this.id = null
+        this.nickname = null
+        this.socket.disconnect()
+    }
+}
+
+module.exports = User
